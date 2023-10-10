a940c700d9c63e100b4e8306f5dbf95298fb6762
Kazuhiro MIKAMI <k.mikami@tecsvc.co.jp>
2023-10-10 18:50:21 +0900

    2023.10.10

--- lua/plugins/config/telescope.lua
+++ lua/plugins/config/telescope.lua
@@ -1,10 +1,4 @@
-local status_ok, telescope = pcall(require, "telescope")
-if not status_ok then
-  return
-end
-
-
-telescope.setup {
+require("telescope").setup{
   defaults = {
     vimgrep_arguments = {
       "rg",
