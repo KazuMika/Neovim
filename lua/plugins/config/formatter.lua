24add5c564bf83161f6fca6127ec94f9841714b7
Kazuhiro MIKAMI <k.mikami@tecsvc.co.jp>
2024-03-25 14:39:54 +0900

    2024.03.25

--- lua/plugins/config/formatter.lua
+++ lua/plugins/config/formatter.lua
@@ -1,4 +1,26 @@
 -- Utilities for creating configurations
+--
+local patch_clangformat_bug = function(f)
+  local o = f()
+  if o.args and type(o.args) == 'table' then
+    local new_args = {}
+    local skip = false
+    for i, v in ipairs(o.args) do
+      if skip then
+        skip = false
+      elseif v == '-assume-filename'
+        and (o.args[i + 1] == "''" or o.args[i + 1] == '""')
+      then
+        skip = true
+      elseif type(v) ~= 'string' or not v:find('^-style=') then
+        table.insert(new_args, v)
+      end
+    end
+    o.args = new_args
+  end
+  return o
+end
+
 local util = require "formatter.util"
 
 -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
@@ -26,6 +48,9 @@ require("formatter").setup {
     bash = {
       require("formatter.filetypes.sh").shellharden,
     },
+    cpp = { patch_clangformat_bug(require('formatter.filetypes.cpp').clangformat) },
+
+
     lua = {
       -- "formatter.filetypes.lua" defines default configurations for the
       -- "lua" filetype
